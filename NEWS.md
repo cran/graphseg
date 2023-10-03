0adb8d5f00ec3ec7ed88d6575ecd6ed1b55be19d
Vivien Goepp <vivien.goepp@gmail.com>
2023-10-03 22:10:02 +0000

    version 0.1.1

---
+++ NEWS.md
@@ -0,0 +1,7 @@
+# graphseg 0.1.1
+
+## Added to 0.1.1
+
+* Changed roxygen alias package wide documentation file: changing the deprecated flag
+`@docType package` for `"_PACKAGE"`.
+
--- R/graphseg-package.R
+++ R/graphseg-package.R
@@ -1,12 +1,14 @@
-#' graphseg: A package for piece-wise constant estimation of graph-based signal
+#' graphseg: A package for piece-wise constant estimation of graph-based signals
 #'
 #' The main function of this package is \code{agraph}, which performs segmentation
 #' of signals on graphs into a piecewise-constant signal.
 #' (the function \code{graph_aridge} is deprecated and kept here for backward
 #' compatibility).
 #'
-#' @seealso The \code{\link{flsa}} package on CRAN, which performs a similar task
+#' See the corresponding paper: \doi{10.48550/arXiv.2206.06752}
 #'
-#' @docType package
-#' @name graphseg
+#' @seealso The \code{\link{flsa}} package on CRAN, which performs a similar task.
+#'
+#' @keywords internal
+"_PACKAGE"
 NULL
